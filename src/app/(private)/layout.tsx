c7522ca480170b9e5e6d32d1faa25235dd84bfa3
Kelvin Lora <kelvinlora5@gmail.com>
2025-06-13 20:19:51 -0400

    update mx h

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,3 +1,4 @@
+import { HomeIcon, PieChartIcon, PlusCircleIcon } from "lucide-react";
 import React, { ReactElement } from "react";
 
 const PrivateLayout = ({
@@ -7,11 +8,23 @@ const PrivateLayout = ({
 }): ReactElement => {
   return (
     <div className="max-h-dvh h-full flex-1 flex flex-col overflow-hidden">
-      <main className="flex-1 overflow-y-auto border-2 bg-blue-500">
-        {children}
-      </main>
+      <main className="flex-1 overflow-y-auto">{children}</main>
 
-      {/* <Navbar /> */}
+      <nav className="h-16 border-t bg-white flex items-center justify-around">
+        <button className="flex flex-col items-center text-sm">
+          <HomeIcon className="w-6 h-6" />
+          Home
+        </button>
+        <button className="flex flex-col items-center text-sm">
+          <PieChartIcon className="w-6 h-6" />
+          Budgets
+        </button>
+        <button className="flex flex-col items-center text-sm">
+          <PlusCircleIcon className="w-6 h-6" />
+          New
+        </button>
+        {/* <LogoutButton /> */}
+      </nav>
     </div>
   );
 };
