b7da89e560ceba39cd1bd946623a2b8e86ffb449
Kelvin Lora <kelvinlora5@gmail.com>
2025-06-15 12:13:34 -0400

    remve unncesary files

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,22 +1,23 @@
-import { LogoutButton } from "@/components/logout-button";
+import { LogoutButton } from "@/components/auth/logout-button";
 import { HomeIcon, PieChartIcon, PlusCircleIcon } from "lucide-react";
+import Link from "next/link";
 import { ReactElement } from "react";
 
 const Navbar = (): ReactElement => {
   return (
     <nav className="h-24 border-t bg-white flex pt-2 justify-around">
-      <button className="flex flex-col items-center text-sm">
+      <Link href="/" className="flex flex-col items-center text-sm">
         <HomeIcon className="w-6 h-6" />
         Home
-      </button>
+      </Link>
       <button className="flex flex-col items-center text-sm">
         <PieChartIcon className="w-6 h-6" />
         Budgets
       </button>
-      <button className="flex flex-col items-center text-sm">
+      <Link href="/new-budget" className="flex flex-col items-center text-sm">
         <PlusCircleIcon className="w-6 h-6" />
         New
-      </button>
+      </Link>
       <LogoutButton />
     </nav>
   );
