3c979e728bc46e8f370ea2f09a9e36d8ca36ce06
Kelvin Lora <kelvinlora5@gmail.com>
2025-07-06 19:30:38 -0400

    fix build errors

--- src/app/(private)/budget_templates/page.tsx
+++ src/app/(private)/budget_templates/page.tsx
@@ -1,5 +1,6 @@
 import BudgetTemplatesShell from "@/components/budget_templates/BudgetTemplatesShell";
 import {
+  fetchBudgetTemplatesServer,
   fetchCategoriesServer,
   fetchExpensesTemplateServer,
 } from "@/lib/supabase/request/server";
@@ -7,8 +8,10 @@ import {
 const BudgetTemplatesPage = async () => {
   const expenseTemplates = await fetchExpensesTemplateServer();
   const categories = await fetchCategoriesServer();
+  const defaultBudgetTemplates = await fetchBudgetTemplatesServer();
   return (
     <BudgetTemplatesShell
+      defaultBudgetTemplates={defaultBudgetTemplates}
       expenseTemplates={expenseTemplates}
       categories={categories}
     />
